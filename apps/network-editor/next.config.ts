62245d7b07c159be42600e296281a4df10c6ee99
Maetee L <may3rd@gmail.com>
2025-12-05 23:57:04 +0700

    fix vercel link

--- apps/network-editor/next.config.ts
+++ apps/network-editor/next.config.ts
@@ -3,17 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@eng-suite/physics", "@eng-suite/ui-kit"],
-  basePath: '/network-editor',
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/network-editor',
-        permanent: false,
-        basePath: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
