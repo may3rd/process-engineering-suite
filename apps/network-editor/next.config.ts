56cb80ef2bbc26904b23e0e76a0e5413b5e53e9c
Maetee L <may3rd@gmail.com>
2025-12-06 00:27:12 +0700

    let see

--- apps/network-editor/next.config.ts
+++ apps/network-editor/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@eng-suite/physics", "@eng-suite/ui-kit"],
+  basePath: "/network-editor",
 };
 
 export default nextConfig;
