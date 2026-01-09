85474f95c9353764d2355ad4e640524d4f6175e3
Maetee L <may3rd@gmail.com>
2026-01-09 10:00:46 +0700

    feat(api): restore design_agents router and deps

--- apps/api/main.py
+++ apps/api/main.py
@@ -70,11 +70,13 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:3002",
-        "http://localhost:3004",  # Design Agents app
+        "http://localhost:3003",
+        "http://localhost:3004",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
         "http://127.0.0.1:3002",
-        "http://127.0.0.1:3004",  # Design Agents app
+        "http://127.0.0.1:3003",
+        "http://127.0.0.1:3004",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
     ],
